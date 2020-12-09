1eaaf200e2edac2e9b782817de5a6de6e34e7c28
Андрій <Mula_jw0a@student.itstep.org>
2020-12-09 17:18:41 +0200

    enddd

--- MVVM_LightKit/ViewModel/RegistrationWindowViewModel.cs
+++ MVVM_LightKit/ViewModel/RegistrationWindowViewModel.cs
@@ -38,17 +38,15 @@ namespace MVVM_LightKit.ViewModel
 
         public void ExecuteRegisterCommand()
         {
-            Repositories.RClients.Add(CurrentClient);
-            System.Windows.Application.Current.Resources["Login"] = CurrentClient.Login;
-            System.Windows.Application.Current.Resources["Password"] = CurrentClient.Password;
-            IsWindowVisible = false;
-            SelectExcursion selectExcursion = new SelectExcursion();
-            selectExcursion.Show();
-        }
-
-        public bool CanExecuteRegisterCommand()
-        {
-            return !string.IsNullOrEmpty(CurrentClient.SName) && !string.IsNullOrEmpty(CurrentClient.PName);
+            if(!string.IsNullOrEmpty(CurrentClient.SName) && !string.IsNullOrEmpty(CurrentClient.PName) && !string.IsNullOrEmpty(CurrentClient.PhoneNumber) && !string.IsNullOrEmpty(CurrentClient.Login) && !string.IsNullOrEmpty(CurrentClient.Password))
+            {
+                Repositories.RClients.Add(CurrentClient);
+                System.Windows.Application.Current.Resources["Login"] = CurrentClient.Login;
+                System.Windows.Application.Current.Resources["Password"] = CurrentClient.Password;
+                IsWindowVisible = false;
+                SelectExcursion selectExcursion = new SelectExcursion();
+                selectExcursion.Show();
+            }
         }
 
 
@@ -84,68 +82,5 @@ namespace MVVM_LightKit.ViewModel
             }
         }
         ////////////////////////////////////////////////////////
-
-        /* public bool CanExecuteRegisterCommand()
-         {
-             if (string.IsNullOrEmpty(CurrentClient.SName) ||
-                 string.IsNullOrEmpty(CurrentClient.PName) ||
-                 string.IsNullOrEmpty(CurrentClient.PhoneNumber) ||
-                 string.IsNullOrEmpty(CurrentClient.Email) ||
-                 CurrentClient.Birthday == null)
-                 return false;
-             return true;
-         }*/
-
-        /*string _textBoxLogin_Text = "Логин";
-        public string TextBoxLogin_Text
-        {
-            get
-            {
-                return _textBoxLogin_Text;
-            }
-            set
-            {
-                _textBoxLogin_Text = value;
-                RaisePropertyChanged("TextBoxLogin_Text");
-            }
-        }
-
-        //OPEN REG WINDOW COMMAND
-        RelayCommand _textBox_Login_GotFocus;
-        public RelayCommand TextBox_Login_GotFocus
-        {
-            get
-            {
-                if (_textBox_Login_GotFocus == null)
-                    _textBox_Login_GotFocus = new RelayCommand(textBox_Login_GotFocus, true);
-                return _textBox_Login_GotFocus;
-            }
-        }
-
-        private void textBox_Login_GotFocus()
-        {
-            if (_textBoxLogin_Text == "Логин")
-                _textBoxLogin_Text = "";
-        }
-        ////////////////////////////////////////////////////////
-
-
-
-        RelayCommand _textBox_Login_LostFocus;
-        public RelayCommand TextBox_Login_LostFocus
-        {
-            get
-            {
-                if (_textBox_Login_LostFocus == null)
-                    _textBox_Login_LostFocus = new RelayCommand(textBox_Login_LostFocus, true);
-                return _textBox_Login_GotFocus;
-            }
-        }
-
-        private void textBox_Login_LostFocus()
-        {
-            if (_textBoxLogin_Text == "")
-                _textBoxLogin_Text = "Логин";
-        }*/
     }
 }
